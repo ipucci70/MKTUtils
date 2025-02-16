b14c6570f3e66097219951fb103033ca97afecfd
Igor Pucci <i.pucci@list-group.com>
2025-02-16 11:34:39 +0100

    first commit

---
+++ src/main/java/org/example/DateTimeUtils.java
@@ -0,0 +1,33 @@
+package org.example.lightadaptercip.utils;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class DateTimeUtils {
+    public static final int JULIAN_CONSTANT = 15018;
+
+    private static final LocalDate START_JULIAN = LocalDate.of(1900, 1, 1);
+
+    public static LocalDate ftDateToLocalDate(int ftDate) {
+        // ftDate: yyyyMMdd
+        return LocalDate.of(ftDate / 10000, (ftDate / 100) % 100, ftDate % 100);
+    }
+
+    // NB the Excel date is computed EXCLUSIVE of ftDate
+    public static int ftDateToExcelDate(int ftDate) {
+        return (int) ChronoUnit.DAYS.between(START_JULIAN, ftDateToLocalDate(ftDate));
+    }
+
+    // NB the FMR date is computed INCLUSIVE of ftDate
+    public static int excelDateToFMRDate(int excelDate) {
+        return excelDate + JULIAN_CONSTANT + 1;
+    }
+
+    // NB the FMR date is computed INCLUSIVE of ftDate
+    public static int ftDateToFMRDate(int ftDate) {
+        return excelDateToFMRDate(ftDateToExcelDate(ftDate));
+    }
+
+    // private constructor to hide the implicit one
+    private DateTimeUtils() { }
+}
---
+++ src/main/java/org/example/Utils.java
@@ -0,0 +1,114 @@
+package org.example.lightadaptercip.utils;
+
+import com.google.common.base.Strings;
+//import it.list.jft.dd.ftxcandeal.r1.FTXCANDEAL;
+//import it.list.jft.dd.ftxcandeal.r1.FT_C_PRICE_INFO;
+//import it.list.jft.dd.ftxcandeal.r1.FT_C_PRICE_STATE;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Utils {
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern("yyyyMMdd");
+    private static final DateTimeFormatter MTIME_FORMATTER =
+            DateTimeFormatter.ofPattern("HHmmssSSS");
+
+    private static final double RANDOM_BLANK_CHANCE = .25;
+
+    public static final double FT_C_NULL_PRICE = 0.0;
+    public static final double FT_C_NULL_YIELD = 9999.0;
+    public static final double FT_C_NULL_RATE = -999999.0;
+    public static final double FT_C_NULL_SPREAD = -999999.0;
+    public static final double FT_C_NULL_NPV = -999999.0;
+    
+    private static final double[] POW_10 = {
+            1.0,
+            10.0,
+            100.0,
+            1000.0,
+            10000.0,
+            100000.0,
+            1000000.0,
+            10000000.0,
+            100000000.0,
+            1000000000.0,
+            10000000000.0
+    };
+
+    public static int getLDateToday() {
+        return Integer.parseInt(DATE_FORMATTER.format(LocalDate.now()));
+    }
+
+    public static int getMTimeNow() {
+        return Integer.parseInt(MTIME_FORMATTER.format(LocalTime.now()));
+    }
+
+    public static double getRandomPrice(double origin, double bound, double tick) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return FT_C_NULL_PRICE;
+        return getRandomDouble(origin, bound, tick);
+    }
+
+    public static double getRandomYield(double origin, double bound) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return FT_C_NULL_YIELD;
+        return getRandomDouble(origin, bound);
+    }
+
+    public static double getRandomSpread(double origin, double bound, double tick) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return FT_C_NULL_SPREAD;
+        return getRandomDouble(origin, bound, tick);
+    }
+
+    public static double getRandomRate(double origin, double bound) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return FT_C_NULL_RATE;
+        return getRandomDouble(origin, bound);
+    }
+
+    public static double getRandomQuantity(double origin, double bound, double tick) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return 0.0;
+        return getRandomDouble(origin, bound, tick);
+    }
+
+    public static double getRandomDouble(double origin, double bound) {
+        return round(ThreadLocalRandom.current().nextDouble(origin, bound), 6);
+    }
+
+    public static double getRandomDouble(double origin, double bound, double tick) {
+        return roundToTick(ThreadLocalRandom.current().nextDouble(origin, bound), tick == 0 ? 0.001 : tick);
+    }
+
+    public static int getRandomInt(int origin, int bound) {
+        if (ThreadLocalRandom.current().nextDouble() < RANDOM_BLANK_CHANCE) return 0;
+        return ThreadLocalRandom.current().nextInt(origin, bound);
+    }
+
+
+    public static int getPrecision(double number) {
+        if (number == 0.001) {
+            return 4; // shortcut for 99.9% of real securities
+        } else if (number <= 1) {
+            return ((int) -Math.log10(number)) + 1;
+        } else {
+            return (int) Math.log10(number);
+        }
+    }
+
+    public static double round(double number, int decimals) {
+        return ((long) (number * POW_10[decimals])) / POW_10[decimals];
+    }
+
+    public static double roundToTick(double number, double tick) {
+        return round(Math.round(number / tick) * tick, getPrecision(tick));
+    }
+
+    public static String stackTraceToString(Throwable e) {
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        return sw.toString();
+    }
+}
